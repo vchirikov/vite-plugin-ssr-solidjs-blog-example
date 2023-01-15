12fb2ed592e9137737180e222ac7dc6d6188a0cc
Vladimir Chirikov <vovanchig@gmail.com>
2023-01-15 16:09:36 +0300

    wip locale

--- src/pages/renderer/_default.page.route.ts
+++ src/pages/renderer/_default.page.route.ts
@@ -1,14 +1,26 @@
 /** @file contains route hooks, works on environment: server, client */
+import { createUrlLocaleDetector } from '#shared/i18n/url-locale-detector';
+import { isBrowser } from '#shared/utils';
+import type { PageContext } from '#types';
 
-// export function onBeforeRoute(pageContext) {
-//   const { urlWithoutLocale, locale } = extractLocale(pageContext.urlOriginal);
-//   return {
-//     pageContext: {
-//       // We make `locale` available as `pageContext.locale`
-//       locale,
-//       // We overwrite `pageContext.urlOriginal`
-//       urlOriginal: urlWithoutLocale
-//     }
-//   };
-// }
+export function onBeforeRoute(pageContext: PageContext) {
+  console.log(pageContext, JSON.stringify(pageContext.exportsAll));
+  const detectors = [];
+  // if (isBrowser()) {
+  //   detectors.push();
+  // }
+  // else {
+
+  // }
+
+  return {
+    pageContext: {
+      // We make `locale` available as `pageContext.locale`
+      locale: 'en',
+      // We overwrite `pageContext.urlOriginal`
+      //urlOriginal: urlWithoutLocale
+      urlOriginal: pageContext.urlOriginal
+    }
+  };
+}
 
